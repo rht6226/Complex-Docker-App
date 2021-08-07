08b38f47f99c6556f5984af9bcd341e73c2e6441
rht6226 <rohitraazanand567@gmail.com>
2021-08-07 21:38:44 +0530

    Completed Frontend

---
+++ frontend/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+           Created a demo Docker application...
+           <Link to="/">Go back home</Link>
+        </div>
+    );
+};
